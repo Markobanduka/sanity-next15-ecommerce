a5e683e1885cd87b8b2c9c7ab06f68f2324970aa
Markobanduka <banduka44@gmail.com>
2024-11-15 23:52:23 +0100

    layout and page restructured

---
+++ app/(store)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div>
+      Home 123
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default Home;
---
+++ app/(store)/search/page.tsx
@@ -0,0 +1,10 @@
+const SearchPage = async ({
+  searchParams,
+}: {
+  searchParams: { query: string };
+}) => {
+  const { query } = await searchParams;
+  return <div>SearchPage for {query}</div>;
+};
+
+export default SearchPage;
